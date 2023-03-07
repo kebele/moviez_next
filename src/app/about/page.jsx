cf0c70ef95ac4e1b1a39fe37472cb3ede25e33af
kebele <51454858+kebele@users.noreply.github.com>
2023-03-07 15:06:37 +0300

    about page added

---
+++ src/app/about/page.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+        excepturi, magnam iste esse vitae illo nam incidunt, aliquid odit modi
+        deserunt iure dolorum! Nemo ipsam quasi hic itaque accusamus aliquid
+        possimus quae ad vel id. Laboriosam, minima, animi magni ratione
+        deleniti tempore dicta placeat, laborum veniam odio esse fuga vitae
+        ducimus quos dolorem rerum reiciendis. Cumque eum error quisquam commodi
+        quis voluptatem aperiam reiciendis ullam nisi non quae, mollitia
+        blanditiis illo exercitationem earum expedita accusantium magni, qui
+        animi? Eos qui eaque unde inventore. Tenetur quibusdam nobis, harum eum
+        rerum similique debitis quam molestias maxime possimus exercitationem ad
+        magnam excepturi consequuntur.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores
+        perspiciatis sed nam, voluptate quaerat ab consectetur doloribus
+        repudiandae vero animi! Deleniti eaque voluptates impedit asperiores
+        repellendus quidem reprehenderit sunt, unde, dolorum ab recusandae quas
+        repellat, odit blanditiis libero est iure quo debitis ipsa accusantium!
+        Fugiat ullam minima accusantium corporis, possimus aut voluptates
+        cupiditate itaque quas magni architecto numquam, laboriosam repellat
+        aperiam tempore, quisquam qui ipsum unde nemo necessitatibus earum
+        nostrum!
+      </p>
+    </div>
+  );
+}
